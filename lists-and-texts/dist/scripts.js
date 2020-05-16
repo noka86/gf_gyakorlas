f03e5960e5f850fa3e28969b16dc1f108b284bdc
Csizmadia Nora <csizmadia.nora86@gmail.com>
2020-05-17 00:47:47 +0200

    trying to solve appending names

--- lists-and-texts/dist/scripts.js
+++ lists-and-texts/dist/scripts.js
@@ -1,3 +1,15 @@
-var names = ["Tomi", "Marci", "Dávid", "Nóri"];
+/*var names = ['Tomi', 'Marci', 'Dávid', 'Nóri'];
+names.forEach(myFunction);
+
+function myFunction(name) {
+    $("ul").append("<li>name</li>");
+} */
 
 $("ul").append("<li>Hello World!</li>");
+
+let names = ['Tomi', 'Marci', 'Dávid', 'Nóri'];
+names.forEach((i) => {
+    $("ul").append("<li>i</li>");
+})
+
+//('names[3]').css('weight', 'bold');
